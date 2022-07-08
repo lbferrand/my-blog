9e967048cf38f3c68828eb194a9d0f0ac73fda6b
lbferrand <lbferrand@gmail.com>
2022-07-08 12:41:11 -0400

    Create more components

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ArticleList from "./pages/ArticlesList";
 import ArticlePage from "./pages/ArticlePage";
+import NavBar from "./NavBar";
 import "./App.css";
 
 const App = () => {
   return (
     <Router>
       <div className="App">
+        <NavBar />
         <div id="page-body">
           <Routes>
-            <Route path="/" element={<HomePage />} />
+            <Route path="/" exact element={<HomePage />} />
             <Route path="/about" element={<AboutPage />} />
             <Route path="/articles-list" element={<ArticleList />} />
-            <Route path="/article" element={<ArticlePage />} />
+            <Route path="/article/:name" element={<ArticlePage />} />
           </Routes>
         </div>
       </div>
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,16 +1,19 @@
 import React from "react";
+import articleContent from "./article-content";
 
-const ArticlePage = () => {
+const ArticlePage = ({ match }) => {
+  const name = match.params.name;
+  const article = articleContent.find((article) => article.name === name);
+
+  if (!article) return <h1>Article does not exist!</h1>;
+
   return (
-    <React.Fragment>
-      <h1>This is an Articles</h1>
-      <p>
-        With software developers and designers located across the United States
-        and Latin America, we offer clients both onshore and nearshore teams.
-        And with Nearshore +, clients can choose a blended team led by a USA
-        based senior engineer, combined with professionals from Latin America.
-      </p>
-    </React.Fragment>
+    <>
+      <h1>{article.title}</h1>
+      {article.content.map((paragraph, key) => (
+        <p key={key}>{paragraph}</p>
+      ))}
+    </>
   );
 };
 
